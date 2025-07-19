9dceaf14b896c4ccb66936c55499b1589a0cd5d4
zxvwp <zxvwp00@gmail.com>
2025-07-19 22:54:08 +0800

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "qxp-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "https://cdn.tailwindcss.com",
+  "https://unpkg.com/dexie@3.2.4/dist/dexie.min.js"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
---
+++ manifest.js
@@ -0,0 +1,22 @@
+
+{
+  "name": "qxp-wxllet",
+  "short_name": "wxllet",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "orientation": "landscape",
+  "icons": [
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
